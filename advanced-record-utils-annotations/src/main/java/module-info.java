022be70dc692a75397687b7fe82c1aff517b0d9d
Conrad <41846367+cbarlin@users.noreply.github.com>
2025-05-02 12:24:08 +1000

    Add some more documentation, fix POM links for maven central

--- advanced-record-utils-annotations/src/main/java/module-info.java
+++ advanced-record-utils-annotations/src/main/java/module-info.java
@@ -1,5 +1,10 @@
 import org.jspecify.annotations.NullMarked;
 
+/**
+ * Dependency for users.
+ * <p>
+ * Basic useage is available at https://github.com/cbarlin/advanced-record-utils
+ */
 @NullMarked
 module io.github.cbarlin.aru.annotations {
 
