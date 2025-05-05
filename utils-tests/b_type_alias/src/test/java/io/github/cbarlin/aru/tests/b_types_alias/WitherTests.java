c40f7ee19926a4547e9b7cda961ce4b534e39ff4
Conrad <41846367+cbarlin@users.noreply.github.com>
2025-05-05 13:02:23 +1000

    Address PR comments

--- utils-tests/b_type_alias/src/test/java/io/github/cbarlin/aru/tests/b_types_alias/WitherTests.java
+++ utils-tests/b_type_alias/src/test/java/io/github/cbarlin/aru/tests/b_types_alias/WitherTests.java
@@ -1,16 +1,27 @@
 package io.github.cbarlin.aru.tests.b_types_alias;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
 
 class WitherTests {
 
-    void canHandleReplacementWithDelaias() {
+    @Test
+    void canHandleReplacementWithDealias() {
         final SomeRecord original = SomeRecordUtils.builder()
             .randomIntA(69)
+            .bookName("This is a bookname")
+            .authorName("This is the author name")
+            .randomIntB(13)
             .build();
-        final SomeRecord after = original.withRandomIntA(42);
+        final SomeRecord after = original.withRandomIntA(42)
+            .withBookName((BookName) null);
 
         assertEquals(69, original.randomIntA().value());
         assertEquals(42, after.randomIntA().value());
+        assertNull(after.bookName());
+        assertEquals(original.authorName(), after.authorName());
+        assertEquals(original.randomIntB(), after.randomIntB());
     }
 }
