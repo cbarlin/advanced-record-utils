31c6fbb1561fbd8a16aa59dab574700ccbc46c34
Conrad <41846367+cbarlin@users.noreply.github.com>
2025-08-04 19:05:37 +1000

    [ci skip] Prepare release 0.6.0

--- advanced-record-utils-annotations/src/main/java/io/github/cbarlin/aru/annotations/AdvancedRecordUtilsGenerated.java
+++ advanced-record-utils-annotations/src/main/java/io/github/cbarlin/aru/annotations/AdvancedRecordUtilsGenerated.java
@@ -43,8 +43,8 @@ public @interface AdvancedRecordUtilsGenerated {
 
     public @interface Version {
         public static final int MAJOR_VERSION = 0;
-        public static final int MINOR_VERSION = 5;
-        public static final int PATCH_VERSION = 1;
+        public static final int MINOR_VERSION = 6;
+        public static final int PATCH_VERSION = 0;
 
         int major() default MAJOR_VERSION;
 
