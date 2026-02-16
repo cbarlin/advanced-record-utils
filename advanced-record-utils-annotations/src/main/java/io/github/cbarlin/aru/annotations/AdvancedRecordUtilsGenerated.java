a43f22a1f55cc9609c99743cd7ad4e45c8561e94
cbarlin <41846367+cbarlin@users.noreply.github.com>
2026-02-16 12:12:16 +1000

    [ci skip] Prepare release 0.7.4

--- advanced-record-utils-annotations/src/main/java/io/github/cbarlin/aru/annotations/AdvancedRecordUtilsGenerated.java
+++ advanced-record-utils-annotations/src/main/java/io/github/cbarlin/aru/annotations/AdvancedRecordUtilsGenerated.java
@@ -255,7 +255,7 @@ public @interface AdvancedRecordUtilsGenerated {
         /**
          * The patch version of the library
          */
-        public static final int PATCH_VERSION = 3;
+        public static final int PATCH_VERSION = 4;
 
         /**
          * The major version of the generator used
